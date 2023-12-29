e516efea9f695d6207a2f90833dc0f4fa456cf5c
casscannelle <cassia.maffei@gmail.com>
2023-12-29 16:22:12 -0300

    inclusão componente welcome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Welcome from './components/welcome/welcome';
 
-function App() {
+function App (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">Hello World div</div>
+    
+      <div className="body"><Welcome /></div>
     </div>
-  );
+  )
 }
 
+
+
 export default App;
+
+
---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Welcome =()=>{
+  return <div>Hello World Component</div>
+  
+}
+
+export default Welcome;
