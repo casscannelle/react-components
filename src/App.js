6c8e5e110ec710f7af06cb6ec7b31596f3aa6c1f
casscannelle <cassia.maffei@gmail.com>
2023-12-29 16:24:06 -0300

    inclusão timestamp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
+import Timestamp from 'react-timestamp';
 import Welcome from './components/welcome/welcome';
 
 function App (){
   return (
     <div className="App">
-      <div className="header">Hello World div</div>
+      <div className="header">Hello World div</div> <Timestamp relative date={Date} autoUpdate />
     
       <div className="body"><Welcome /></div>
     </div>
