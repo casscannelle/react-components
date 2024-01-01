e4f5176a8015e88ddbb1665101d376f7105dccc4
casscannelle <cassia.maffei@gmail.com>
2024-01-01 18:12:24 -0300

    estilos css

--- src/App.js
+++ src/App.js
@@ -9,20 +9,18 @@ function App (){
     let [titleText, setTitleText] = useState("Hello World (8)");
     const bodyText = 'Hello World (7)';
 
-    
-  
   return (
     <div className="App">
 
       <div className="header">Hello World (1)</div>
-      <div className="body"><Welcome /></div>
+      <div className="body-text"><Welcome /></div>
       <div className="text">Hello World (3)</div>
       <div className="paragraph">Hello World (4)</div>
       <div className="quote">Hello World (5)</div>
       <div className="note"><OlaMundo /></div>
       <div className="function">{bodyText}</div>
       <div className="input"> {titleText}</div>
-      <div className="container" id="container"><button onClick={Hello}>Clique Aqui</button></div>
+      <div className="container" id="container"><button onClick={Hello}>Clique Aqui  para o 9</button></div>
       <div className="footer">Hello World (10)</div>
 
 
